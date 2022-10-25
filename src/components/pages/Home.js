f3e543d804ff1433578e06dddf5db6083f65e200
Nife16 <100443715+Nife16@users.noreply.github.com>
2022-10-25 15:03:25 -0500

    stuff from 10/25

---
+++ src/components/pages/Home.js
@@ -0,0 +1,24 @@
+import '../../css/components/home.css'
+
+const Home = () => {
+
+
+    return (
+        <div className='flex-col page-container'>
+            <div className='flex-row header'>
+                <div className='flex-col third-width'>
+
+                </div>
+                <div className='flex-col third-width'>
+
+                </div>
+                <div className='flex-col third-width'>
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
